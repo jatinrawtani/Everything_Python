0a01c61b66ddb5ddd3d8e78ad7bc4bc34abd22b9
jatin <jatinrawtani@yahoo.com>
2021-10-27 01:35:12 +0530

    27/10/21

---
+++ py4e/Using the GeoJSON API.py
@@ -0,0 +1,14 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+
+target = 'http://py4e-data.dr-chuck.net/json?'
+local = input('enter location:')
+url = target + urllib.parse.urlencode({'address': local, 'key' : 42})
+
+print('Retrieving', url)
+data = urllib.request.urlopen(url).read()
+print('Retrieved', len(data), 'characters')
+js = json.loads(data)
+print(json.dumps(js, indent=4))
+print('place id',js['results'][0]['place_id'])
